b7a462fa8dfe6694a4224f590fe9b96a8c2a4ac6
Feer93 <fernando.serrano444@gmail.com>
2021-04-02 18:31:58 +0200

    api

---
+++ bbdd/API/peticiones.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const connection = require('./database.js');
+
+// Settings
+app.set('port', process.env.PORT || 3050);
+
+app.use(cors());
+
+// Middlewares
+app.use(express.json());
+
+
+app.use(express.urlencoded({extended: true}));
+
+
+// Starting the server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
+
+app.post('/register',(req,res)=>{
+
+    const sql = "INSERT into usuario SET ?";
+
+    const usuario = {
+        email : req.body.email,
+        password: req.body.password,
+        nickname: req.body.nickname,
+        points: 0,
+        coins: 0,
+    }
+
+    connection.query("SELECT * FROM usuario WHERE nickname='"+usuario.nickname+"'",(error,result)=>{
+
+        if (error) throw error;
+        if (result.length > 0) {
+            res.json({
+                message: 'Ya existe un usuario con ese nickname'
+            })
+        }else {
+           // let hash = bcrypt.hashSync(usuarioObj.contraseña, saltRounds);
+            //usuarioObj.contraseña=hash;
+            connection.query(sql, usuarioObj, error => {
+                if (error) throw error;
+                res.json({
+                    message: 'Usuario registrado!'
+                });
+            });
+        }
+
+    });
+
+
+})
+
+app.post('/login',(req,res)=>{
+
+    connection.query("SELECT * FROM usuario WHERE nickname='"+usuario.nickname+"'"+"and password='"+usuario.password+"'",(error,result)=>{
+
+        if (error) throw error;
+        if (result.length > 0) {
+            res.json({
+                message: 'Login sucesfully.'
+            })
+        }else {
+           
+            res.json({
+                message: 'Login not sucesfully.'
+            })
+        }
+
+    });
+
+
+})
