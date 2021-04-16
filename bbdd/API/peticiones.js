0b18d66e62ff95b592dd1ba53b680bc48347aaf4
asolanas0000 <andrea.solanasvicente@gmail.com>
2021-04-16 15:13:33 +0200

    añadido para obtener elementos de tienda

--- bbdd/API/peticiones.js
+++ bbdd/API/peticiones.js
@@ -395,3 +395,20 @@ app.post('/FinalIndividual_Usuario',(req,res)=>{
         }
     });
 })
+
+app.post('/PantallaTienda',(req,res)=>{
+    
+    connection.query("SELECT * FROM item",(error,result)=>{
+    
+        if (result.length > 0) {
+            console.log(result);
+            res.json(result);
+            
+        }else {
+            if (error) throw error;
+            res.status(400).json({
+                message: 'No se han podido obtener los items'
+            }) 
+        }
+    });
+})
